8923005e3e112af13d3ba0efd966470410cf15e4
sangaray <scgaraycochea@yahoo.com.ar>
2023-12-04 12:14:15 -0300

    Creación Sidebar

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined,
+} from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import profileImage from "assets/profile.jpeg";
+
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,12 +3,25 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar"
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)")
+  const isSidebarOpen, setisSibarOpen = useState(true)
+
   return (
-    <Box width="100%" height="100%">
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar 
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setisSibarOpen={isSidebarOpen}
+      />
       <Box>
-        <Navbar />
+        <Navbar 
+        isSidebarOpen={isSidebarOpen}
+        setisSibarOpen={isSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
