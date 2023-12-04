8c569b555eb4ce3b69a414638d740e73760ad31e
sangaray <scgaraycochea@yahoo.com.ar>
2023-12-04 12:15:58 -0300

    Creación Sidebar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,25 +3,22 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
-import Sidebar from "components/Sidebar"
+import Sidebar from "components/Sidebar";
 
 const Layout = () => {
-  const isNonMobile = useMediaQuery("(min-width: 600px)")
-  const isSidebarOpen, setisSibarOpen = useState(true)
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setisSibarOpen] = useState(true);
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
-      <Sidebar 
+      <Sidebar
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
         setisSibarOpen={isSidebarOpen}
       />
       <Box>
-        <Navbar 
-        isSidebarOpen={isSidebarOpen}
-        setisSibarOpen={isSidebarOpen}
-        />
+        <Navbar isSidebarOpen={isSidebarOpen} setisSibarOpen={isSidebarOpen} />
         <Outlet />
       </Box>
     </Box>
