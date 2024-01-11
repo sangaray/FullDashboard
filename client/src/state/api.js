3f28311dd267c22d43daa36aaf11ce306a57977c
sangaray <scgaraycochea@yahoo.com.ar>
2024-01-11 15:13:29 -0300

    Conctando llamando un usuario a la sidebar

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -2,4 +2,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/quiery/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: ["User"],
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => "general/user/${id}",
+      providesTags: ["User"],
+    }),
+  }),
 });
+export const { UseGetUserQuery } = api;
